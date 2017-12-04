b8792eb81472e67ac2ad2bb26ef6a4e542aa91b7
SoumenITMO <soumenbanerjeenik@gmail.com>
2017-12-05 01:54:41 +0400

    Del

--- src/main/java/spring/test/soumen/controllers/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package spring.test.soumen.controllers.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import spring.test.soumen.controllers.model.Users;
-
-public interface UsersRepository extends JpaRepository<Users, Integer>
-{
-
-}
